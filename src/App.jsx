e6c71bb08e118e40a3b2bf44fe313cacf7e1bae6
kbhavre78 <kbhavre78@gmail.com>
2025-02-04 12:57:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Ques1 from './components/Ques1'
+
+const App = () => {
+  return (
+    <div>
+    <Ques1/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Ques1.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const Ques1 = () => {
+
+    const [count, setCount] = useState(0);
+
+
+  return (
+    <div className=' bg-red-200 '>
+      <h1 className='text-4xl px-4 py-2 text-center'>Question 1 : Counter App</h1>
+        <div className='flex flex-col items-center my-5'> 
+            <h1 className='text-2xl'>{count}</h1>
+
+            <div className='flex gap-3 my-4'>
+            <button className='bg-purple-400 px-4 py-1 rounded-md ' onClick={()=>setCount(count+1)}>Increment</button>
+            <button className='bg-purple-400 px-4 py-1 rounded-md ' onClick={()=> setCount(count-1)}>Decrement</button>
+            </div>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Ques1
